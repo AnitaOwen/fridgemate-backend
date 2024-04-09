cbf2a004ed0df96f961a55c5792b58a3ac37220f
AnitaOwen <anitaowen@pursuit.org>
2024-04-08 20:57:43 -0400

    Show route is working for fridges

--- controllers/fridgesController.js
+++ controllers/fridgesController.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
 const fridges = express.Router();
-const { getAllFridgesByUserId } = require("../queries/fridges")
+const { getAllFridgesByUserId, getOneFridge } = require("../queries/fridges")
 
-fridges.get('/', async (req,res) => {
-    // http://localhost:3003/api/fridges?user=1
-    const  userId  = req.query.user
-    if(!userId) res.status(400).json({ error: "User ID is required." })
-    const allFridges = await getAllFridgesByUserId(userId);
+//INDEX
+fridges.get('/:user_id', async (req,res) => {
+    // http://localhost:3003/api/fridges/1
+    const { user_id } = req.params
+    if(!user_id) res.status(400).json({ error: "User ID is required." })
+    const allFridges = await getAllFridgesByUserId(user_id);
 
     if(allFridges[0]){
         res.status(200).json(allFridges);
@@ -15,5 +16,19 @@ fridges.get('/', async (req,res) => {
         res.status(500).json({ error: "User not found."})
     }
 });
+
+//SHOW
+fridges.get('/:user_id/:fridge_id', async (req, res) => {
+    const { user_id, fridge_id } = req.params
+
+    const oneFridge = await getOneFridge(fridge_id, user_id)
+    if(oneFridge) {
+        res.status(200).json(oneFridge)
+    } else {
+        res.status(418).json({ error: 'Fridge not found'})
+    }
+
+})
+
 
 module.exports = fridges
