5bbf379d5f49e9d65a034b032921b4713943627b
AnitaOwen <anitaowen@pursuit.org>
2024-04-08 22:42:16 -0400

    added update route for items - not tested

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -3,10 +3,9 @@ const express = require("express");
 const {
     getAllItems,
     getOneItem,
-    // createReview,
-    // deleteReview,
-    // updateReview,
-    // grabUserAndReview,
+    createItem,
+    deleteItem,
+    updateItem
   } = require("../queries/items.js");
 
 const { getOneFridge } = require("../queries/fridges.js");
@@ -43,6 +42,46 @@ items.get("/:item_id", async (req, res) => {
       res.status(500).json({ error: "Item not found" });
     }
   });
+
+// CREATE http://localhost:3003/api/fridges/1/2/items
+items.post("/", async (req, res) => {
+    const { fridge_id } = req.params;
+    const newItem = await createItem({ ...req.body, fridge_id });
   
+    if (newItem.id) {
+      res.status(200).json(newItem);
+    } else {
+      res.status(500).json({ error: "Failed to create new item." });
+    }
+  });
+
+// DELETE http://localhost:3003/api/fridges/1/2/items/13
+items.delete("/:item_id", async (req, res) => {
+    const { item_id } = req.params;
+
+    const deletedItem = await deleteItem(item_id);
+  
+    if (deletedItem.id) {
+      res.status(200).json(deletedItem);
+    } else {
+      res.status(404).json({ error: "Item not found" });
+    }
+  });
+
+// UPDATE http://localhost:3003/api/fridges/1/2/items/13
+items.put("/:item_id", async (req, res) => {
+    const { user_id, fridge_id, item_id } = req.params;
+    const updatedItem = await updateItem({
+      ...req.body,
+      user_id,
+      fridge_id,
+      item_id
+    });
+    if (updatedItem.id) {
+      res.status(200).json(updatedItem);
+    } else {
+      res.status(404).json({ error: "Item not found" });
+    }
+  });
 
   module.exports = items;
--- queries/items.js
+++ queries/items.js
@@ -23,12 +23,67 @@ const getOneItem = async (itemId) => {
       return error;
     }
   };
+
+// CREATE
+const createItem = async (item) => {
+    try {
+      const newItem = await db.one(
+        "INSERT INTO items (name, expiration_date, amount_paid, fridge_id, user_id, category_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+        [
+          item.name,
+          item.expiration_date,
+          item.amount_paid,
+          item.fridge_id,
+          item.user_id,
+          item.category_id
+        ]
+      );
+      return newItem;
+    } catch (error) {
+      return error;
+    }
+  };
+
+// DELETE
+const deleteItem = async (itemId) => {
+    try {
+      const deletedItem = await db.one(
+        "DELETE FROM items WHERE id = $1 RETURNING *",
+        itemId
+      );
+      return deletedItem;
+    } catch (error) {
+      return error;
+    }
+  };
+
+// UPDATE
+const updateItem = async (item) => {
+    try {
+      const updatedItem = await db.one(
+        "UPDATE items SET name=$1, expiration_date=$2, amount_paid=$3, fridge_id=$4, user_id=$5, category_id=$6 WHERE id=$7 RETURNING *",
+        [
+          item.name,
+          item.expiration_date,
+          item.amount_paid,
+          item.fridge_id,
+          item.user_id,
+          item.category_id,
+          item.item_id
+        ]
+      );
+      return updatedItem;
+    } catch (error) {
+      return error;
+    }
+  };
   
 
   module.exports = {
     getAllItems,
     getOneItem, 
-    // deleteReview,
-    // updateReview,
+    createItem,
+    deleteItem,
+    updateItem
   };
   
