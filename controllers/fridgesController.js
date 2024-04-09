3b92245a6d5f2cc58804d74ced74d30ca78a4342
AnitaOwen <anitaowen@pursuit.org>
2024-04-08 21:43:13 -0400

    created delete and update routes for fridges

--- controllers/fridgesController.js
+++ controllers/fridgesController.js
@@ -1,11 +1,16 @@
 const express = require("express");
 
 const fridges = express.Router();
-const { getAllFridgesByUserId, getOneFridge } = require("../queries/fridges")
+const { 
+    getAllFridgesByUserId, 
+    getOneFridge, 
+    createFridge, 
+    deleteFridge,
+    updateFridge
+} = require("../queries/fridges")
 
-//INDEX
+//INDEX http://localhost:3003/api/fridges/1
 fridges.get('/:user_id', async (req,res) => {
-    // http://localhost:3003/api/fridges/1
     const { user_id } = req.params
     if(!user_id) res.status(400).json({ error: "User ID is required." })
     const allFridges = await getAllFridgesByUserId(user_id);
@@ -17,7 +22,7 @@ fridges.get('/:user_id', async (req,res) => {
     }
 });
 
-//SHOW
+//SHOW http://localhost:3003/api/fridges/1/2
 fridges.get('/:user_id/:fridge_id', async (req, res) => {
     const { user_id, fridge_id } = req.params
 
@@ -30,5 +35,45 @@ fridges.get('/:user_id/:fridge_id', async (req, res) => {
 
 })
 
+// CREATE http://localhost:3003/api/fridges/1
+fridges.post("/:user_id",  async (req, res) => {
+    const { user_id } = req.params;
+    // console.log(req.body, user_id)
+    const newFridge = await createFridge({ ...req.body, user_id });
+  
+    if (newFridge.id) {
+      res.status(200).json(newFridge);
+    } else {
+      res.status(500).json({ error: "Failed to add new fridge." });
+    }
+  });
+
+//  DELETE http://localhost:3003/api/fridges/1/2
+fridges.delete("/user_id/:fridge_id", async (req, res) => {
+    const { user_id, fridge_id } = req.params;
+  
+    const deletedFridge = await deleteFridge(fridge_id, user_id);
+  
+    if (deletedFridge) {
+      res.status(200).json(deletedFridge);
+    } else {
+      res.status(404).json({ error: "Fridge not found" });
+    }
+  });
+
+// UPDATE http://localhost:3003/api/fridges/1/2
+reviews.put("/user_id/:fridge_id", async (req, res) => {
+    const { user_id, fridge_id } = req.params;
+    const updatedFridge = await updateFridge({
+      fridge_id,
+      ...req.body,
+      user_id,
+    });
+    if (updatedFridge.id) {
+      res.status(200).json(updatedFridge);
+    } else {
+      res.status(404).json({ error: "Fridge not found" });
+    }
+  });
 
 module.exports = fridges
--- queries/fridges.js
+++ queries/fridges.js
@@ -1,6 +1,6 @@
 const db = require("../db/dbConfig.js")
 
-//Index
+// INDEX
 const getAllFridgesByUserId = async (userId) => {
     try {
         const allFridges = await db.any(`SELECT * FROM fridges WHERE fridges.user_id = $1`, userId);
@@ -10,7 +10,7 @@ const getAllFridgesByUserId = async (userId) => {
     }
 };
 
-//Show
+// SHOW
 const getOneFridge = async (fridgeId, userId) => {
     try {
         const oneFridge = await db.one(`SELECT fridges.* FROM fridges JOIN users ON fridges.user_id = users.id WHERE fridges.id = $1 AND users.id = $2`, [fridgeId, userId])
@@ -20,4 +20,60 @@ const getOneFridge = async (fridgeId, userId) => {
     }
 }
 
-module.exports = {getAllFridgesByUserId, getOneFridge}
+// CREATE
+const createFridge = async (fridge) => {
+    try {
+      const newFridge = await db.one(
+        "INSERT INTO fridges (location, notes, user_id) VALUES($1, $2, $3) RETURNING *",
+        [
+          fridge.location,
+          fridge.notes, 
+          fridge.user_id
+        ]
+      );
+      return newFridge;
+    } catch (error) {
+      return error;
+    }
+  };
+
+// DELETE 
+const deleteFridge = async (fridgeId, userId) => {
+    try {
+      const deletedFridge = await db.one(
+        "DELETE FROM fridges WHERE id = $1 AND user_id = $2 RETURNING *",
+        [fridgeId, userId]
+      );
+      return deletedFridge;
+    } catch (error) {
+      return error;
+    }
+  };
+
+// UPDATE 
+const updateFridge = async (fridge) => {
+    try {
+      const updatedFridge = await db.one(
+        "UPDATE fridge SET location=$1, notes=$2 WHERE id=$3 AND user_id=$4 RETURNING *",
+        [
+          fridge.location,
+          fridge.notes,
+          fridge.fridge_id,
+          fridge.user_id
+        ]
+      );
+      return updatedFridge;
+    } catch (error) {
+      return error;
+    }
+  };
+  
+
+
+module.exports = { 
+    getAllFridgesByUserId, 
+    getOneFridge, 
+    createFridge, 
+    deleteFridge, 
+    updateFridge
+}
